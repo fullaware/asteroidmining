a1e3bca2e1944d1d8c703431d1b1ba3a6279dddb
Full Aware <fullaware@@users.noreply.github.com>
2024-11-05 21:35:21 -0500

    Fixed build and values

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ docker build . -t fullaware/asteroidmining:latest
 docker push fullaware/asteroidmining:latest
 
 helm upgrade asteroidmining ./asteroidmining/ -n asteroidmining --create-namespace
-kubectl rollout restart deploy asteroidmining -n asteroidmining
+kubectl rollout restart deploy asteroidmining-app -n asteroidmining
